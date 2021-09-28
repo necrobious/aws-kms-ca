2e5135f4cfa08425c6d80c59a3e56ca7241afc97
Kirk Peterson <necrobious@gmail.com>
2021-09-27 19:23:02 -0700

    adding missing ext impl

--- src/certificate/extensions/mod.rs
+++ src/certificate/extensions/mod.rs
@@ -5,14 +5,35 @@ pub mod subject_key_identifier;
 pub mod extended_key_usage;
 pub mod subject_alternative_name;
 
-use yasna::{DERWriter, DEREncodable};
-use yasna::models::ObjectIdentifier;
+use yasna::{
+    ASN1Result,
+    DERWriter,
+    DEREncodable,
+    BERReader,
+    BERDecodable,
+    models::ObjectIdentifier,
+};
 
 #[derive(Clone)]
 pub struct Extension {
     oid: ObjectIdentifier,
     critical: bool,
     value: Vec<u8>,
+}
+
+impl BERDecodable for Extension {
+    fn decode_ber(reader: BERReader) -> ASN1Result<Self> {
+        reader.read_sequence(|reader| {
+            let oid = reader.next().read_oid()?;
+            let critical = reader.next().read_bool()?;
+            let value = reader.next().read_bytes()?;
+            Ok(Extension {
+                oid: oid,
+                critical: critical,
+                value: value,
+            })
+        })
+    }
 }
 
 impl DEREncodable for Extension {
